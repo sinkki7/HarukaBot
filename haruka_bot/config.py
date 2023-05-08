2f6257812815caaf7a69e5974a89dfdb3535117d
sinkki <love674706574@gmail.com>
2023-05-09 00:59:09 +0800

    fix(动态渲染)：修复了配置文件

--- haruka_bot/config.py
+++ haruka_bot/config.py
@@ -23,6 +23,8 @@ class Config(BaseSettings):
     haruka_command_prefix: str = ""
     # 频道管理员身份组
     haruka_guild_admin_roles: List[str] = ["频道主", "超级管理员"]
+    # 使用DynamicRender渲染动态图片
+    use_dynamic_render: bool = True
 
     @validator("haruka_interval", "haruka_live_interval", "haruka_dynamic_interval")
     def non_negative(cls, v: int, field: ModelField):
